4716910d96ed587a9a0e2cf1da4df1f05dffed18
Matheus Mascarenhas <mattmascarenhas7@gmail.com>
2023-07-05 23:40:44 -0300

    text...

---
+++ Controllers/TextoController.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using Geradoc.Domain.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace geradoc_v2.Controllers {
+    public class TextoController : Controller {
+        private readonly Database _context;
+
+        public TextoController(Database context) {
+            _context = context;
+        }
+
+        //listar todos
+        [HttpGet("v1/texts")]
+        public IEnumerable<Texto> GetAll() {
+            return _context
+                   .Connection
+                   .Query<Texto>("SELECT * FROM [Textos]");
+        }
+    }
+}
--- Entities/Texto.cs
+++ Entities/Texto.cs
@@ -1,14 +1,17 @@
 ﻿using geradoc_v2.Entities.Shared;
 
 namespace Geradoc.Domain.Entidades {
-    public class Textos: Entity {
-        public Textos(string titulo, string texto) {
+    public class Texto: Entity {
+        public Texto(string titulo, string texto) {
             this.Titulo = titulo;
-            this.Texto = texto;
+            this.Conteudo = texto;
             DataDeCriacao = DateTime.Now;
+            DataDeAtualizacao = DateTime.Now;
+        }
+        public Texto() {
         }
         public string Titulo { get; private set; }
-        public string Texto { get; private set; }
+        public string Conteudo { get; private set; }
         public DateTime DataDeCriacao { get; private set; }
         public DateTime DataDeAtualizacao { get; private set; }
 
